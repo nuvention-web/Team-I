cf2e857835459b8aada4a0a47e8c0ba7c7668ab6
shk172 <sangkim2017@u.northwestern.edu>
2017-02-23 21:28:51 -0600

    Button works, leads to an alternate view of the card with details of the events

--- Activid8/app/components/home/home.ios.js
+++ Activid8/app/components/home/home.ios.js
@@ -1,22 +1,65 @@
-// app/components/home.ios.js
+// app/components/home.android.js /// NOT TESTED
 
-import React from "react";
-import {StyleSheet, Text, View, Image} from "react-native";
+import React, {Component} from "react";
+import {StyleSheet, Text, View, Image, Alert, Button, ScrollView} from "react-native";
 import SwipeCards from "react-native-swipe-cards";
 // import reactMixin from "react-mixin";
 // import HomeCommon from "./home.common";
 
+/*
+TODO: Create a button to expand a card
+*/
 
-let Card = React.createClass({
-  render() {
-    return (
-      <View style={styles.card}>
-        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
-        <Text style={styles.text}>This is card {this.props.name}</Text>
-      </View>
+
+class Card extends Component{
+  constructor(props){
+    super(props);
+    this.state = {expanded: false};
+  }
+
+  expandCard = () => {
+    this.setState(
+      {expanded: !this.state.expanded}
     );
   }
-});
+
+  render() {
+    if(this.state.expanded){
+      return (
+          <ScrollView>
+            <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+            <Text>Name: {this.props.name}</Text>
+            <Text>Age: {this.props.age} </Text>
+            <Text>Bio: {this.props.bio} </Text>
+            <Text>Event: {this.props.eventTitle}</Text>
+            <Text>Event Details: This will have details about the event</Text>
+            <Button
+                onPress={this.expandCard}
+                title="Go back"
+                color="#FF8900"
+                accessibilityLabel="Learn more about this event"
+              />
+            </ScrollView>
+        );
+    }
+    else{
+      return (
+          <View style={styles.card}>
+            <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+            <Text style={styles.text}>{this.props.name}, {this.props.age}</Text>
+            <Text style={styles.text}> {this.props.eventTitle}</Text>
+            <Button
+                onPress={this.expandCard}
+                title="Details"
+                color="#FF8900"
+                accessibilityLabel="Learn more about this event"/>
+            </View>
+        );
+    }
+
+
+  }
+}
 
 let NoMoreCards = React.createClass({
   render() {
@@ -29,10 +72,10 @@ let NoMoreCards = React.createClass({
 });
 
 const Cards = [
-  {name: "1", image: "https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif"},
-  {name: "2", image: "https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif"},
-  {name: "3", image: "https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif"},
-  {name: "4", image: "https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif"},
+  {name: "Neil", age: "58", bio: "Scientist. Smart.", eventTitle: "Want to go to the planetarium?", image: "http://i.imgur.com/GuAB8OE.jpg"},
+  {name: "Greg", age: "32", bio: "Hi", eventTitle: "Going to watch La La Land before it's out of the theater", image: "http://i.imgur.com/6ePfo5i.jpg"},
+  {name: "Bryan", age: "21", bio: "Nose looks like someone twerking.", eventTitle: "Anyone wanna hit up Chipotle?", image: "http://i.imgur.com/VtmB3LX.jpg"},
+  {name: "Cynthia", age: "27", bio: "A New York artist", eventTitle:"Field Museum free weekend - who's in?", image: "http://www.newwaytoship.com/wp-content/uploads/2015/02/banner-person-maria.png"},
   {name: "5", image: "https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif"},
   {name: "6", image: "https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif"},
   {name: "7", image: "https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif"},
@@ -50,20 +93,20 @@ const Cards2 = [
 
 
 const Home = React.createClass({
-  getInitialState() {
-    return {
-      cards: Cards,
-      outOfCards: false
-    };
-  },
-  handleYup (card) {
-    console.log("yup");
-  },
-  handleNope (card) {
-    console.log("nope");
-  },
-  cardRemoved (index) {
-    console.log("The index is ${index}");
+    getInitialState() {
+      return {
+          cards: Cards,
+          outOfCards: false
+      };
+    },
+    handleYup (card) {
+      console.log("yup");
+    },
+    handleNope (card) {
+      console.log("nope");
+    },
+    cardRemoved (index) {
+      console.log("The index is ${index}");
 
     let CARD_REFRESH_LIMIT = 3;
 
@@ -105,14 +148,31 @@ const Home = React.createClass({
 
 const styles = StyleSheet.create({
   card: {
+    width: 300,
+    height: 400,
     alignItems: "center",
     borderRadius: 5,
     // overflow: "hidden",
-    borderColor: "grey",
+    borderColor: "black",
     backgroundColor: "white",
     borderWidth: 1,
-    elevation: 1
+    elevation: 1,
+    paddingBottom: 10
   },
+
+  expandedCard: {
+    width: 300,
+    height: 800,
+    alignItems: "center",
+    borderRadius: 5,
+    // overflow: "hidden",
+    borderColor: "black",
+    backgroundColor: "white",
+    borderWidth: 1,
+    elevation: 1,
+    paddingBottom: 10
+  },
+
   thumbnail: {
     flex: 1,
     width: 240,
@@ -123,6 +183,8 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10
   },
+
+
   noMoreCards: {
     flex: 1,
     justifyContent: "center",
@@ -130,8 +192,8 @@ const styles = StyleSheet.create({
   },
   container:{
     backgroundColor:"#fff",
-    height:200,
-    width:200
+    width:300,
+    height: 400
   },
 });
 
