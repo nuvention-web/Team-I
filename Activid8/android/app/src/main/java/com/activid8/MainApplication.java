0190e3a1ecb50687036d675499e862b086b7fe11
shk172 <sangkim2017@u.northwestern.edu>
2017-04-14 10:50:47 -0500

    [Android] Fb Auth works on Android

--- Activid8/android/app/src/main/java/com/activid8/MainApplication.java
+++ Activid8/android/app/src/main/java/com/activid8/MainApplication.java
@@ -5,6 +5,9 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -16,6 +19,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -26,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage()
+              new FBSDKPackage(mCallbackManager)
       );
     }
   };
