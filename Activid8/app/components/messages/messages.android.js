b46a8b81ac27f1cf8c155a907cba02e9c9fee22c
SangHee Kim <sangkim2017@u.northwestern.edu>
2017-05-08 00:27:21 -0500

    Working on chatboxes
    Work in progress

--- Activid8/app/components/messages/messages.android.js
+++ Activid8/app/components/messages/messages.android.js
@@ -10,9 +10,37 @@ class Messages extends Component {
 
   	render(){
   		return(
-  			<View style = {styles.viewContainer}>
-  			<Text> Work in progress.. </Text>
-  			</View>
+	      <View style={styles.container}>
+	        <View style={styles.topContainer}>
+	          <TouchableHighlight
+	            underlayColor={'#4e4273'}
+	            onPress={this.onBackPress}
+	            style={{marginLeft: 15}}
+	            >
+	            <Text style={{color: '#fff'}}>&lt; Back</Text>
+	          </TouchableHighlight>
+	        </View>
+	        <View style={styles.chatContainer}>
+	          <Text style={{color: '#000'}}>Chat</Text>
+	        </View>
+	        <View style={styles.inputContainer}>
+	          <View style={styles.textContainer}>
+	            <TextInput
+	              style={styles.input}
+	              value={this.state.message}
+	              onChangeText={(text) => this.setState({message: text})}
+	              />
+	          </View>
+	          <View style={styles.sendContainer}>
+	            <TouchableHighlight
+	              underlayColor={'#4e4273'}
+	              onPress={() => this.onSendPress()}
+	              >
+	              <Text style={styles.sendLabel}>SEND</Text>
+	            </TouchableHighlight>
+	          </View>
+	        </View>
+	      </View>
   		);
   	}
 }
@@ -20,8 +48,56 @@ class Messages extends Component {
 export default Messages;
 
 
-const styles = StyleSheet.create({
-  viewContainer: {
-    ...Platform.select({ios: {top: 129},android: {top: 119}}),
-  },
-});
+var styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      backgroundColor: '#ffffff'
+    },
+    topContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      backgroundColor: '#6E5BAA',
+      paddingTop: 20,
+    },
+    chatContainer: {
+      flex: 11,
+      justifyContent: 'center',
+      alignItems: 'stretch'
+    },
+    inputContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: '#6E5BAA'
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: 'center'
+    },
+    sendContainer: {
+      justifyContent: 'flex-end',
+      paddingRight: 10
+    },
+    sendLabel: {
+      color: '#ffffff',
+      fontSize: 15
+    },
+    input: {
+      width: windowSize.width - 70,
+      color: '#555555',
+      paddingRight: 10,
+      paddingLeft: 10,
+      paddingTop: 5,
+      height: 32,
+      borderColor: '#6E5BAA',
+      borderWidth: 1,
+      borderRadius: 2,
+      alignSelf: 'center',
+      backgroundColor: '#ffffff'
+    },
+  });
