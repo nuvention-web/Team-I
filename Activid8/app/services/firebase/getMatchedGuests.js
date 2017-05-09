fd7b5551c432f8e5dabeba6291e9ce088edd81df
shk172 <sangkim2017@u.northwestern.edu>
2017-05-09 00:11:28 -0500

    Fixed the aforementioned bug
    The matched users load properly in the messages tab

--- Activid8/app/services/firebase/getMatchedGuests.js
+++ Activid8/app/services/firebase/getMatchedGuests.js
@@ -1,5 +1,6 @@
 import getUserID from "../facebook/getUserID";
 import firebaseApp from "./firebaseService";
+import getFirebaseUser from "./getFirebaseUser";
 import newFirebaseUser from "./newFirebaseUser";
 
 
@@ -12,8 +13,19 @@ export default function getFirebaseSelf () {
       userRef.on("value", function(snapshot) {
         var exists = (snapshot.val().matched_users !== null);
         if (exists) {
-          // console.log(snapshot.val());
-          resolve(snapshot.val().matched_users);
+          var guestList = [];
+          for(var i in snapshot.val().matched_users){
+            getFirebaseUser(snapshot.val().matched_users[i]).then(
+              (guest)=>{
+                guestList.push(guest);
+                console.log(guest);
+              },
+              (err)=>{
+                console.log(err);
+              }
+            );
+          }
+          resolve(guestList);
         }
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
